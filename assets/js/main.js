e8585de5b3d146518ac1cb1ad331091030819d44
nipunaupeksha <nipuna.upeksha@gmail.com>
2022-06-05 15:20:02 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/*========== SHOW MENU ==========*/
+
+/*===== MENU SHOW =====*/
+
+/*===== MENU HIDDEN =====*/
+
+/*========== REMOVE MOBILE MENU ==========*/
+
+/*========== CHANGE BACKGROUND HEADER ==========*/
+
+/*========== SCROLL SECTIONS ACTIVE LINK ==========*/
+
+/*========== SHOW SCROLL UP ==========*/
+
+/*========== SWIPER JS ==========*/
+
+/*========== SCROLL REVEAL JS ==========*/
